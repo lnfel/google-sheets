54e802bc0b78d0f0bd1e68f9a25a5186e3e08410
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-09-29 20:56:13 +0800

    [Feature] AppShell

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
-	import '../app.css'
+	import { afterNavigate } from '$app/navigation'
+	import '../app.css';
+
+	import { AppShell } from '@skeletonlabs/skeleton'
+
+	afterNavigate((params) => {
+		const isNewPage = params.from && params.to && params.from.route.id !== params.to.route.id
+		const elemPage = document.querySelector('#page')
+		if (isNewPage && elemPage !== null) {
+			elemPage.scrollTop = 0
+		}
+	});
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="pageHeader">Page Header</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
